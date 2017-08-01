183c3c6d218cebf10daf7a2c144e603d90ee4691
ajohnson7006 <ajohnson7006@gmail.com>
2017-08-01 16:51:48 -0400

    First Round Page Ideas.

---
+++ header.php
@@ -0,0 +1,40 @@
+<?php
+#This is the primary header file to be loaded on all webpages.
+
+#include db_connect.php;
+?>
+<html>
+	<head>
+		<title>The Tech and Travel Group</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="Tech Travel QA Engineering AWS Amazon Web Services" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+	</head>
+
+<!-- Header -->
+  <header id="header" class="skel-layers-fixed">
+    <h1><a href="#">Ion</a></h1>
+    <nav id="nav">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact Us</a></li>
+        <!-- <li><a href="left-sidebar.html">Left Sidebar</a></li>    -->
+        <!-- <<li><a href="right-sidebar.html">Right Sidebar</a></li> -->
+        <!-- <li><a href="no-sidebar.html">No Sidebar</a></li>        -->
+        <li><a href="#" class="button special">Sign Up</a></li>
+      </ul>
+    </nav>
+  </header>
+</html>
