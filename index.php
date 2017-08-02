702fef14e879b920a9d15be0b7f4595a17fc9de5
rinafey <rinadventures@gmail.com>
2017-08-01 21:03:13 -0600

    test

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include "./header.php";
 		<!-- Banner -->
 			<section id="banner">
 				<div class="inner">
-					<h2>This is a Code Merge.</h2>
+					<h2>Change</h2>
 					<!--<h2>The Tech and Travel Group</h2>-->
 					<p>Where A Great Tag Line Collaborates </p>
 					<ul class="actions">
